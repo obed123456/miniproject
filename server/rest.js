0f5e4aacfdfb76c6decc8e45e425aa0394f4fdf5
obed123456 <obedsam0022013@gmail.com>
2018-03-22 14:54:15 +0100

    add fetch and update endpoints

--- server/rest.js
+++ server/rest.js
@@ -12,9 +12,10 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
 
 //Adding users in databas
 //POST request
+//this  will add new student in db
 router.post("/addstudents",function(req,res){
     var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
-    var table = ["student","fname","lname","age",req.body.fname,req.body.lname,req.body.age];
+    var table = ["student","sFname","lname","age",req.body.fname,req.body.lname,req.body.age];
     query = mysql.format(query,table);
     connection.query(query,function(err,rows){
         if(err) {
@@ -25,6 +26,68 @@ router.post("/addstudents",function(req,res){
         }
     });
 });
+
+//this will get student by
+router.get("/getallclasses",function(req,res){
+    var query = "SELECT ??,  ??,  ?? FROM (((?? INNER  JOIN ?? ON ?? = ??) INNER JOIN ?? ON ?? = ??) INNER JOIN ?? ON ?? = ??)";
+    var table = ["student.sFname","teacher.tFname","classes.danceClasses","allClasse", "student", "student.studentID","allClasse.studentID","teacher", "teacher.teacherID","allClasse.teacherID","classes","classes.classesID","allClasse.classID",req.body.fname,req.body.lname,req.body.age];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if(err) {
+          throw (err);
+            // res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+// get all street dance lessons and totalstudents and date.
+router.get("/getstreet",function(req,res){
+    var query = "SELECT COUNT ()??,  ??,  ?? FROM (((?? INNER  JOIN ?? ON ?? = ??) INNER JOIN ?? ON ?? = ??) INNER JOIN ?? ON ?? = ??)";
+    var table = ["student.sFname", "classes.date","allClasse", "student", "student.studentID","allClasse.studentID","teacher", "teacher.teacherID","allClasse.teacherID","classes","classes.classesID","allClasse.classID",req.body.fname,req.body.lname,req.body.age];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if(err) {
+          throw (err);
+            // res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+// get all street dance lessons and totalstudents and date.
+router.get("/getstreet",function(req,res){
+    var query = "SELECT * FROM (allClasse INNER JOIN classes ON classes.classesID = allClasse.classID)";
+    var table = ["student.sFname", "classes.date","allClasse", "student", "student.studentID","allClasse.studentID","teacher", "teacher.teacherID","allClasse.teacherID","classes","classes.classesID","allClasse.classID",req.body.fname,req.body.lname,req.body.age];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if(err) {
+          throw (err);
+            // res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+// get all street dance lessons and totalstudents and date.
+router.get("/getallclasseswithname",function(req,res){
+    var query = "SELECT student.sFname, teacher.tFname, classes.danceClasses, classes.date, classes.start, classes.length FROM (((allClasse INNER JOIN student ON student.studentID = allClasse.studentID) INNER JOIN teacher ON teacher.teacherID = allClasse.teacherID) INNER JOIN classes ON classes.classesID = allClasse.classID);";
+    var table = ["student.sFname", "teacher.tFname", "classes.danceClasses", "classes.start","classes.date", "classes.length", "allClasse", "student", "student.studentID","allClasse.studentID","teacher", "teacher.teacherID","allClasse.teacherID","classes","classes.classesID","allClasse.classID"];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if(err) {
+          throw (err);
+            // res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.json({"Users": rows});
+        }
+    });
+});
+
+
 
 }
 
